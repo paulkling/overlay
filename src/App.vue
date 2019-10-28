fd374b1a18bffab6ab6ab7d13b12525191c1b6f8
Paul Kling <pkling@gmail.com>
2019-10-28 17:43:06 -0500

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld question="Welcome to Your Vue.js App" helper="Type !vote abcde" time="23s"/>
   </div>
 </template>
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,8 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="hello" id="page">
+    <question>{{ question }}</question>
+    <time>{{ time }}</time>
+    <helper>{{ helper }}</helper>
   </div>
 </template>
 
@@ -34,25 +10,75 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    question: String,
+    time: String,
+    helper: String
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+#page {
+  display: grid;
+  width: 35%;
+  grid-template-areas: "question question time"
+                       "helper helper helper"
+                       "option text text";
+  grid-template-rows: 50px 35px 30px;
+  grid-template-columns: 20px 1fr 50px ;
+  font: 15px arial;
+  background-color: #0a0a0a;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+#page > time {
+  grid-area: time;
+  color: #fcfcfc;
+  background-color: #42a832;
+  text-align: center;
+  padding: 15px 0;
+ 
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+#page > question {
+  grid-area: question;
+  color: #fcfcfc;
+  background-color: #42a832;
+  text-align: center;
+  padding: 15px 0;
 }
-a {
-  color: #42b983;
+#page > helper {
+  grid-area: helper;
+  color: #fcfcfc;
+  background-color: #0a0a0a;
+  align-content: center;
+  text-align: left;
+  padding: 5px 0;
+  font-style: italic;
 }
+
+#page > option {
+  color: #fcfcfc;
+  background-color: #787878;
+  border: 1px solid #0a0a0a;
+  text-align: center;
+  padding: 4px 0;
+}
+
+#page > text {
+  color: #fcfcfc;
+  background-color: #787878;
+  border: 1px solid #0a0a0a;
+  padding: 4px 10px;
+  width: 50%
+
+}
+
+#page > vote {
+  color: #f3ff12;
+  text-align: right;
+  padding: 5px 8px;
+}
+
+
 </style>
